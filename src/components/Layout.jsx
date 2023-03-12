f844e75b89db0765287d8cfce744c804bbba5008
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-12 01:48:04 +0100

    clean

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Header from './Header'
 
 const Layout = ({ children }) => (
   <>
-    <Header siteTitle="Gabriel Software" />
+    <Header />
     <div
       style={{
         margin: '0 auto',
