52ab4fcb1ccd2faeab9e98ce0f08420ac61bccd1
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-14 22:17:33 +0100

    remove no more needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 import prefetch from "@astrojs/prefetch";
@@ -7,5 +6,5 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabriel-software.ch',
-  integrations: [react(), sitemap(), prefetch()]
+  integrations: [sitemap(), prefetch()]
 });
