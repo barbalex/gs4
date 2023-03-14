a82608b86e73dbc165c1a1628ba33dfd455da815
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-14 14:57:03 +0100

    work on carousel

--- src/components/Projekte/_vmCarouselItems.js
+++ src/components/Projekte/_vmCarouselItems.js
@@ -1,29 +1,8 @@
-import vm01 from '/vm_01.jpg'
-import vm02 from '/vm_02.png'
-import vm03 from '/vm_03.png'
 import vm04 from '/vm_04.png'
 import vm05 from '/vm_05.png'
 import vm06 from '/vm_06.png'
 
 export default [
-  {
-    src: vm01,
-    altText: 'Home',
-    caption: '',
-    header: 'Home',
-  },
-  {
-    src: vm02,
-    altText: 'Alle Objekte - Desktop',
-    caption: '',
-    header: 'Alle Objekte - Desktop',
-  },
-  {
-    src: vm03,
-    altText: 'Alle Objekte - mobile',
-    caption: '',
-    header: 'Alle Objekte - mobile',
-  },
   {
     src: vm04,
     altText: 'Art - Desktop',
