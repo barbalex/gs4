b27463d3d39e32e2ac493c81a2bdc605fcf3204e
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-23 22:21:59 +0100

    correct test base url

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,40 +1,40 @@
 import { test, expect } from '@playwright/test'
 
-const baseUrl = 'http://localhost:3000' // one of: 'http://localhost:3000', 'https://gabriel-software.ch'
+const baseUrl = '/' // one of: 'http://localhost:3000', 'https://gabriel-software.ch'
 
 test('has title', async ({ page }) => {
-  await page.goto(baseUrl)
+  await page.goto(`/`)
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Gabriel Software/)
 })
 
 test('Leitbild link', async ({ page }) => {
-  await page.goto(baseUrl)
+  await page.goto(`/`)
   await page.getByRole('link', { name: 'Leitbild' }).click()
   await expect(page).toHaveURL(/.*leitbild/)
 })
 
 test('Projekte link', async ({ page }) => {
-  await page.goto(baseUrl)
+  await page.goto(`/`)
   await page.getByRole('link', { name: 'Projekte' }).click()
   await expect(page).toHaveURL(/.*projekte/)
 })
 
 test('Technologien link', async ({ page }) => {
-  await page.goto(baseUrl)
+  await page.goto(`/`)
   await page.getByRole('link', { name: 'Technologien' }).click()
   await expect(page).toHaveURL(/.*technologien/)
 })
 
 test('Kontakt link', async ({ page }) => {
-  await page.goto('http://localhost:3000')
+  await page.goto(`/`)
   await page.getByRole('link', { name: 'Kontakt' }).click()
   await expect(page).toHaveURL(/.*kontakt/)
 })
 
 test('Projekte sidenav mediterranean-migration', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'mediterranean- migration.com' }).click()
   await expect(
     page.getByRole('link', { name: 'mediterranean-migration.com' }),
@@ -42,7 +42,7 @@ test('Projekte sidenav mediterranean-migration', async ({ page }) => {
 })
 
 test('Projekte sidenav arteigenschaften', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'arteigenschaften.ch' }).click()
   await expect(
     page.getByRole('heading', { name: 'arteigenschaften.ch' }),
@@ -50,13 +50,13 @@ test('Projekte sidenav arteigenschaften', async ({ page }) => {
 })
 
 test('Projekte sidenav apflora', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'apflora.ch' }).click()
   await expect(page.getByRole('heading', { name: 'apflora.ch' })).toBeVisible()
 })
 
 test('Projekte sidenav AWEL-Indikatoren', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'AWEL-Indikatoren' }).click()
   await expect(
     page.getByRole('heading', { name: 'AWEL-Indikatoren' }),
@@ -64,7 +64,7 @@ test('Projekte sidenav AWEL-Indikatoren', async ({ page }) => {
 })
 
 test('Projekte sidenav vermehrung', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'vermehrung.ch' }).click()
   await expect(
     page.getByRole('heading', { name: 'vermehrung.ch' }),
@@ -72,7 +72,7 @@ test('Projekte sidenav vermehrung', async ({ page }) => {
 })
 
 test('Projekte sidenav AWEL-Personal', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'AWEL-Personal' }).click()
   await expect(
     page.getByRole('heading', { name: 'AWEL-Personal' }),
@@ -80,7 +80,7 @@ test('Projekte sidenav AWEL-Personal', async ({ page }) => {
 })
 
 test('Projekte sidenav Kapla', async ({ page }) => {
-  await page.goto(`${baseUrl}/projekte`)
+  await page.goto(`/projekte`)
   await page.getByRole('link', { name: 'Kapla' }).click()
   await expect(page.getByRole('heading', { name: 'Kapla' })).toBeVisible()
 })
