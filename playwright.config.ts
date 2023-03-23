f747d6ed7a966c2b2638caf7871eb442183e1a47
Alexander Gabriel <alex.barbalex@gmail.com>
2023-03-23 22:44:56 +0100

    edit base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,10 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://gabriel-software.ch',
+    baseURL:
+      process.env.NODE_ENV === 'dev'
+        ? 'http://localhost:3000'
+        : 'https://gabriel-software.ch',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
